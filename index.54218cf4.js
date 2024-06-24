e7c90ef371e9f7f1865dad3bbe4645b452eb0376
Denis Reznik <reznik.d.o12@gmail.com>
2024-06-24 03:10:11 +0300

    make build

---
+++ index.54218cf4.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),l=0;l<e.rows.length;l++){var o=e.rows[l],r=o.cells[1].cloneNode(!0);o.cells[o.cells.length-1].before(r)}
+//# sourceMappingURL=index.54218cf4.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const table = document.querySelector("table");
+for(let i = 0; i < table.rows.length; i++){
+    const row = table.rows[i];
+    const newCell = row.cells[1].cloneNode(true);
+    row.cells[row.cells.length - 1].before(newCell);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
